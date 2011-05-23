2d6734f290640aed8dceb410598f92c66c011511
Adam Lehenbauer <adam.lehenbauer@gmail.com>
2011-05-22 20:13:15 -0400

    adds ability to find the config file and option to override it

--- obsidian/ostart.py
+++ obsidian/ostart.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 
+import os
+
+# ./.obs.cnf and ~/.obs.cnf
+project_conf_name = '.obs.cnf'
+# /etc/obs.cnf
+system_conf_name = '/etc/obsidian.cnf'
+
 """
 Starts a Java program defined by file 'cnf'.
 """
@@ -7,29 +14,40 @@ def ostart():
     import sys
     import argparse
     import ConfigParser
-    import os
 
     parser = argparse.ArgumentParser(description='Starts the process named by \'appid\'')
     parser.add_argument('appid', metavar='appid', type=str)
     parser.add_argument('--dry-run', '-d', action='store_true')
+    parser.add_argument('--file', '-f', dest='conf_file', type=str)
 
     args = parser.parse_args()
     appid = args.appid
+    conf_file = args.conf_file
 
+    if conf_file:
+        conf_file = open(conf_file, 'r')
+    else:
+        conf_file = find_conf_file()
+
+    if not conf_file:
+        raise ConfigError('could not find config file')
+        
     if appid:
-        print "starting [%s]" % sys.argv[1]
+        print "starting [%s]" % appid
     else:
         print "appid required"
         exit(1)
 
     config = ConfigParser.ConfigParser()
-    config.read('cnf')
+    #config.readfp(conf_file)
+    config.read(conf_file.name)
 
     # get the config file defaults out first, and recreate
     # the config, providing those
     defaults = dict(config.items('default'))
     config = ConfigParser.ConfigParser(defaults)
-    config.read('cnf')
+    #config.readfp(conf_file)
+    config.read(conf_file.name)
 
     java_cmd = config.get(appid, 'java_home') + "/bin/java"
     classpath = config.get(appid, 'classpath')
@@ -50,17 +68,52 @@ def ostart():
     else:
         app_args = []
 
-    args = jvm_opts
-    args.append("-classpath")
-    args.append(classpath)
-    args.extend(system_opts)
-    args.append(class_)
-    args.extend(app_args)
+    c_args = [java_cmd]
+    c_args.extend(jvm_opts)
+    c_args.append("-classpath")
+    c_args.append(classpath)
+    c_args.extend(system_opts)
+    c_args.append(class_)
+    c_args.extend(app_args)
 
     #print " ".join(args)
+    if args.dry_run:
+       print " ".join(c_args)
+       return
 
-    os.execv(java_cmd, args)
+    os.execv(java_cmd, c_args)
+
+def find_conf_file():
+    conf_file = find_in_parent_dir(project_conf_name)
+    if(conf_file):
+        return conf_file
+
+    conf_file = os.path.expanduser(os.path.join('~', project_conf_name))
+    if os.path.exists(conf_file):
+        return open(conf_file, 'r')
+
+    if os.path.exists(system_conf_name):
+        return open(system_conf_name, 'r')
+        
+    return None
+
+def find_in_parent_dir(fname):
+    p = os.path.abspath(os.path.curdir)
+    
+    while not os.path.exists(os.path.join(p, project_conf_name)):
+        oldp, p = p, os.path.dirname(p)
+        if p == oldp:
+            return None
+    
+    return open(os.path.join(p, project_conf_name), 'r') 
+
+class ConfigError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
 
 if __name__ == "__main__":
-    ostart()
+    try:
+        ostart()
+    except ConfigError as e:
+        print e.msg
 
